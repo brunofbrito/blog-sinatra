d123d46895adf552b9f3bf18c0089bac118e1312
Bruno Brito <brunofbrito@gmail.com>
2015-07-20 10:51:28 +0100

    Adds Add Blog feature and displays it on homepage

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,17 @@
 get "/" do
+  @posts = Post.all
   erb :index
 end
 
-get "/posts" do
-  @posts = Post.all
-  erb :"posts/index"
+get "/new/" do
+  erb :addnew
+end
+
+post "/new/" do
+  post = Post.new({
+      title: params[:title],
+      content: params[:content]
+    })
+  post.save
+  redirect "/"
 end
