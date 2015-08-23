ae7d60d0983424508033e7569f0b78d1a58b8d1f
DouglasAllen <kb9agt@gmail.com>
2015-08-23 12:58:06 -0500

    index.erb change for blank content

--- db/migrate/20150720103752_create_comments.rb
+++ db/migrate/20150720103752_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    t.belongs_to :post, index: true
-    t.string :title
-    t.text :content
-    t.timestamps
-  end
+      t.belongs_to :post, index: true
+      t.string :title
+      t.text :content
+      t.timestamps
+    end
   end
 end
